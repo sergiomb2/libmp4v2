850e1a659866d7cdf656a60afeb694ce54c7f760
eddygroenendaal <eddygroenendaal@6e6572fa-98a6-11dd-ad9f-f77439c74b79>
2009-07-13 23:04:45 +0000

    BRANCHED: RELEASE 1.9.0

---
+++ releases/1.9.1/vstudio9.0/include/mp4v2/project.h
@@ -0,0 +1,30 @@
+#ifndef MP4V2_PROJECT_H
+#define MP4V2_PROJECT_H
+
+/*****************************************************************************/
+
+#define MP4V2_PROJECT_name            "MP4v2"
+#define MP4V2_PROJECT_name_lower      "mp4v2"
+#define MP4V2_PROJECT_name_upper      "MP4V2"
+#define MP4V2_PROJECT_name_formal     "MP4v2 1.9.0"
+#define MP4V2_PROJECT_url_website     "http://code.google.com/p/mp4v2"
+#define MP4V2_PROJECT_url_downloads   "http://code.google.com/p/mp4v2/downloads/list"
+#define MP4V2_PROJECT_url_discussion  "http://groups.google.com/group/mp4v2"
+#define MP4V2_PROJECT_irc             "irc://irc.freenode.net/handbrake"
+#define MP4V2_PROJECT_bugreport       "<eddyg@myreflection.org>"
+#define MP4V2_PROJECT_version         "1.9.0"
+#define MP4V2_PROJECT_version_hex     0x00010900
+#define MP4V2_PROJECT_version_major   1
+#define MP4V2_PROJECT_version_minor   9
+#define MP4V2_PROJECT_version_point   0
+#define MP4V2_PROJECT_repo_url        "https://mp4v2.googlecode.com/svn/releases/1.9.0"
+#define MP4V2_PROJECT_repo_root       "https://mp4v2.googlecode.com/svn"
+#define MP4V2_PROJECT_repo_uuid       "6e6572fa-98a6-11dd-ad9f-f77439c74b79"
+#define MP4V2_PROJECT_repo_rev        332
+#define MP4V2_PROJECT_repo_date       "2009-05-21 14:39:25 -0400 (Thu, 21 May 2009)"
+#define MP4V2_PROJECT_repo_type       "stable"
+#define MP4V2_PROJECT_build           "Thu May 21 14:41:21 EDT 2009"
+
+/*****************************************************************************/
+
+#endif /* MP4V2_PROJECT_H */
