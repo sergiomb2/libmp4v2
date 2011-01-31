4cf1c2de995c4e2e164e1d85e1eacd06649a63f5
dbyron0 <dbyron0@6e6572fa-98a6-11dd-ad9f-f77439c74b79>
2011-01-31 20:41:56 +0000

    merge r443 from trunk -DB

--- libplatform/io/FileSystem_win32.cpp
+++ libplatform/io/FileSystem_win32.cpp
@@ -1,5 +1,6 @@
-#include "libplatform/impl.h"
 #include "src/impl.h"
+#include "libplatform/impl.h" /* for platform_win32_impl.h which declares Utf8ToFilename */
+#include <windows.h>
 
 namespace mp4v2 {
     using namespace impl;
